5f5667fa091b32c2dcc93ecf3339f433596d9f66
srivallikomma <49111660+srivallikomma@users.noreply.github.com>
2020-02-22 16:37:38 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,35 @@
+
+function geturl(){
+    var url = document.getElementById("urlinput").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if(!protocol_ok){
+        newurl = "http://"+url;
+        return newurl;
+        }else{
+            return url;
+        }
+}
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+function myFunction() {
+  var str = geturl(); 
+  var res = str.replace(str,extractHostname(str));
+  var result = res.link(str);
+  document.getElementById("demo").innerHTML = result;
+}
